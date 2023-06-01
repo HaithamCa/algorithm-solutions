bf82130fee85f1a3c548cff47a694482e2920fff
hytham defrawy <hythamdefrawy@gmail.com>
2023-05-31 22:58:06 -0400

    Solve Palindrome

--- palindrome.js
+++ palindrome.js
@@ -3,5 +3,19 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    
+    if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+    let reverse = 0;
+    let original = x;
+
+    while( x > 0 ) {
+        
+        const lastDigit = x % 10;
+
+        reversed = reversed * 10 + lastDigit;
+
+        x = Math.floor(x / 10);
+    }
+
+    return reverse === original;
 };
